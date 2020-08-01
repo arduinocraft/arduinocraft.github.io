a213ef0cc80cf7b86f7af830c4aeee980526512b
gabrielkheisa <gabrielkheisa@gmail.com>
2020-08-01 15:19:32 +0700

    tambah main js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var judul = "@arduino_craft";
+var tahun = currentTime.getFullYear();
+document.getElementById("judulweb").innerHTML = judul;
+document.getElementById("yearfooter").innerHTML = tahun + " " + judul;
